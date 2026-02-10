8a731ed420fb2603adc76ef48be0d578c7408e20
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-02-10 11:34:41 +0530

    the sum of two nos

---
+++ Practice_Code/sum.py
@@ -0,0 +1,13 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Sum", a+b)
+
+
+
+
+
+
+# a=1
+# b=2
+# c = a + b
+# print("The sum of", a, "and", b, "is:", c)
